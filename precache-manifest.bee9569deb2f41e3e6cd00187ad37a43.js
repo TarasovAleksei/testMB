1dc9209d11019eff5fd71e084db040404fdcbd7d
Alex <tarasovas1988@mail.ru>
2021-11-12 14:26:53 +0300

    Updates

---
+++ precache-manifest.bee9569deb2f41e3e6cd00187ad37a43.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "283e86c4e5e0645e6c9652734160a0e3",
+    "url": "/testMB/index.html"
+  },
+  {
+    "revision": "37e6505d63c29808d6ba",
+    "url": "/testMB/static/css/main.a8452051.chunk.css"
+  },
+  {
+    "revision": "2d1b3466be2d0db140a6",
+    "url": "/testMB/static/js/2.9fb708c3.chunk.js"
+  },
+  {
+    "revision": "282cad4058e83221804611932e7ca1ed",
+    "url": "/testMB/static/js/2.9fb708c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37e6505d63c29808d6ba",
+    "url": "/testMB/static/js/main.c0f93fd5.chunk.js"
+  },
+  {
+    "revision": "d2db781b0a7b0f2d3c49",
+    "url": "/testMB/static/js/runtime-main.938fd5d8.js"
+  },
+  {
+    "revision": "522b4953638bacd3ef6d89795ff80d54",
+    "url": "/testMB/static/media/close.522b4953.svg"
+  }
+]);
